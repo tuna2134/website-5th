0ba1b98ab1253e1bc90d19c109b3ada80628d33a
tuna2134 <tuna2134@neody.land>
2024-06-08 12:14:00 +0900

    一応メインページ完成

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,77 @@
+"use client";
+import Header from "@/components/ui/Header";
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
 import Image from "next/image";
+import { VscGithubAlt } from "react-icons/vsc";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaDiscord } from "react-icons/fa";
+
+const socialMedia = [
+    {
+        url: "https://github.com/tuna2134",
+        icon: VscGithubAlt,
+    },
+    {
+        url: "https://twitter.com/fdc_tuna2134",
+        icon: FaXTwitter,
+    },
+    {
+        url: "https://discord.gg/7Z9rZ4X",
+        icon: FaDiscord,
+    },
+];
+
+const avatarURL =
+    "https://cdn.discordapp.com/avatars/739702692393517076/689da5236709ab59a0371b7abcac7bda.png?size=1024";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    const [hidden, setHidden] = useState(true);
+    useEffect(() => {
+        const scrollHandler = () => {
+            if (window.scrollY !== 0) {
+                setHidden(false);
+            } else {
+                setHidden(true);
+            }
+        };
+        window.addEventListener("scroll", scrollHandler);
+        return () => {
+            window.removeEventListener("scroll", scrollHandler);
+        };
+    }, []);
+    return (
+        <AnimatePresence>
+            {!hidden && (
+                <Header
+                    className="fixed top-0 border-b shadow"
+                    animate={true}
+                />
+            )}
+            <div className="flex h-screen w-full items-center justify-center">
+                <div>
+                    <Image
+                        className="mb-2 rounded-full"
+                        alt="avatar.url"
+                        src={avatarURL}
+                        width={156}
+                        height={156}
+                    />
+                    <p className="text-center text-2xl font-bold">tuna2134</p>
+                    <div className="mt-4 flex justify-center space-x-4">
+                        {socialMedia.map((social, key) => (
+                            <a key={key} href={social.url}>
+                                <social.icon size={32} />
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <main className="mx-auto max-w-5xl">
+                <div>
+                    <p>hello</p>
+                </div>
+            </main>
+        </AnimatePresence>
+    );
 }
---
+++ components/ui/Header/index.tsx
@@ -0,0 +1,67 @@
+"use client";
+import Link from "next/link";
+import { tv } from "tailwind-variants";
+import { motion } from "framer-motion";
+import React from "react";
+
+const links = [
+    {
+        name: "works",
+        href: "/works",
+    },
+    {
+        name: "articles",
+        href: "/articles",
+    },
+];
+
+const headerVariant = tv({
+    base: "w-full h-20 flex items-center bg-white",
+});
+
+interface HeaderProps {
+    className?: string;
+    animate?: boolean;
+}
+
+const HeaderComponent: React.FC<{
+    children: React.ReactNode;
+    className: string;
+    animate: boolean;
+}> = ({ animate, children, className }) => {
+    if (animate) {
+        return (
+            <motion.header
+                className={className}
+                initial={{ y: -100 }}
+                animate={{ y: 0 }}
+                exit={{ y: -100 }}
+            >
+                {children}
+            </motion.header>
+        );
+    }
+    return <header className={className}>{children}</header>;
+};
+
+const Header: React.FC<HeaderProps> = ({ className = "", animate = false }) => {
+    const headerClass = headerVariant({ className });
+    return (
+        <HeaderComponent className={headerClass} animate={animate}>
+            <div className="mx-auto w-full max-w-6xl px-4">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-xl">tuna2134</h1>
+                    <nav className="flex space-x-6 rounded-full bg-indigo-600 px-6 py-2 text-white">
+                        {links.map((link, key) => (
+                            <Link key={key} href={link.href}>
+                                {link.name}
+                            </Link>
+                        ))}
+                    </nav>
+                </div>
+            </div>
+        </HeaderComponent>
+    );
+};
+
+export default Header;
