eb151748ab0ba228d1e873d3fe0b0f3080c6bae9
コマリン親衛隊 <tuna2134@neody.land>
2024-06-27 07:35:46 +0000

    fixed

--- components/ui/Header/index.tsx
+++ components/ui/Header/index.tsx
@@ -48,7 +48,7 @@ const Header: React.FC<HeaderProps> = ({ className = "", animate = false }) => {
     const headerClass = headerVariant({ className });
     return (
         <HeaderComponent className={headerClass} animate={animate}>
-            <div className="mx-auto w-full max-w-6xl px-4">
+            <div className="mx-auto w-full max-w-4xl px-4">
                 <div className="flex items-center justify-between">
                     <h1 className="text-xl">tuna2134</h1>
                     <nav className="flex items-center space-x-6 rounded-full bg-indigo-600 px-6 py-2 text-white">
