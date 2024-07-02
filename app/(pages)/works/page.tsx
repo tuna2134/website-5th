ac5b2cb92147da0a77387afda3e77b47b2a2dbc5
tuna2134 <masato@priv.tuna2134.dev>
2024-07-02 10:32:55 +0000

    fix mobile

--- app/(pages)/works/page.tsx
+++ app/(pages)/works/page.tsx
@@ -9,7 +9,7 @@ const Page: NextPage = () => {
             <p className="mt-4">
                 ここでは僕が今まで作って(書いて)きた作品を展示しています。
             </p>
-            <div className="mt-8 grid grid-cols-2 rounded bg-slate-400/10 px-6 py-12">
+            <div className="mt-8 grid grid-cols-1 rounded bg-slate-400/10 px-6 py-12 sm:grid-cols-2">
                 {works.map((work, index) => (
                     <div key={index} className="p-4">
                         <Image
